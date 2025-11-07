8af9fb19786eb3156c147f72447170c47be57daa
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:58:18 +0700

    Menambahkan NavHost pada AplikasiPendaftaran dengan startDestination SelamatDatang

--- app/src/main/java/com/example/questnavigastugas_082/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_082/Navigasi.kt
@@ -1,7 +1,11 @@
 package com.example.questnavigastugas_082
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Tampilan {
@@ -14,5 +18,13 @@ enum class Tampilan {
 fun AplikasiPendaftaran(
     navController: NavHostController = rememberNavController()
 ) {
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = com.example.navigasi.Tampilan.SelamatDatang.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
 
+        }
+    }
 }

26bef993032c840fc7d867286e40ec404a074c13
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:01:51 +0700

    Menambahkan tombol Submit dengan aksi onNextButtonClicked

--- app/src/main/java/com/example/questnavigastugas_082/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/HomePage.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,5 +41,12 @@ fun HalamanUtama(
         Text("20230140082", style = MaterialTheme.typography.bodyMedium)
 
         Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onNextButtonClicked
+        ) {
+            Text("SUBMIT")
+        }
     }
 }

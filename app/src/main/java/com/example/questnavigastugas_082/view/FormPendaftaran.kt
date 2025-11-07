de00a914deea67a4b674c76476d71ea749e96912
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:44:58 +0700

    Menambahkan composable FormulirPendaftaran dengan parameter onSubmitButtonClicked

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigastugas_082.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormulirPendaftaran(
+    onSubmitButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}

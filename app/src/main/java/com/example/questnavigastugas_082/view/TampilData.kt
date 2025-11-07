0e120224fc9348b72622fc56af4b25b754b92ef1
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:29:37 +0700

    Menambahkan Card untuk menampilkan nama lengkap peserta

--- app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
@@ -1,6 +1,7 @@
 package com.example.questnavigastugas_082.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,11 +10,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,6 +54,23 @@ fun DaftarData(
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Bold
             )
+        }
+
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            // Nama Lengkap
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text("NAMA LENGKAP", fontWeight = FontWeight.SemiBold, color = Color.Gray)
+                    Text(nama, style = MaterialTheme.typography.bodyLarge, fontFamily = FontFamily.Cursive)
+                }
+            }
         }
     }
 }

4a5f0d662e3798011f93a628be410321b120b0cf
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:54:21 +0700

    Menambahkan tombol Submit dengan aksi onSubmitButtonClicked

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -22,9 +24,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.questnavigastugas_082.R
 
 @Composable
 fun FormulirPendaftaran(
@@ -120,9 +124,18 @@ fun FormulirPendaftaran(
                     label = { Text("Alamat") },
                     modifier = Modifier.fillMaxWidth()
                 )
+                Spacer(modifier = Modifier.height(24.dp))
 
+                Button(
+                    onClick = onSubmitButtonClicked,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF8A2BE2))
+                ) {
+                    Text(stringResource(R.string.submit), color = Color.White)
+                }
             }
         }
-
     }
 }

d2ca5a78c9bd76456090023888bbc9fe3c8a667b
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:56:47 +0700

    Menambahkan composable AplikasiPendaftaran dengan NavHostController

--- app/src/main/java/com/example/questnavigastugas_082/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_082/Navigasi.kt
@@ -1,7 +1,18 @@
 package com.example.questnavigastugas_082
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Tampilan {
     SelamatDatang,
     DaftarPeserta,
     Formulir
+}
+
+@Composable
+fun AplikasiPendaftaran(
+    navController: NavHostController = rememberNavController()
+) {
+
 }

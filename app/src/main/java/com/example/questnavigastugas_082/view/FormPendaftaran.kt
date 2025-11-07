dbc8b87149732158ff31ff5f10482cdf47ac61f2
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:47:44 +0700

    Merevisi layout Column dan inisialisasi list jenisList & statusList

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -1,12 +1,16 @@
 package com.example.questnavigastugas_082.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -14,13 +18,15 @@ fun FormulirPendaftaran(
     onSubmitButtonClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    // Pilihan data untuk radio button
+    val jenisList = listOf("Laki-laki", "Perempuan")
+    val statusList = listOf("Janda", "Lajang", "Duda")
 
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFF6E6FF))
+            .verticalScroll(rememberScrollState())
         ) {
 
     }

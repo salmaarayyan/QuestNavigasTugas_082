e6e2f4e1d3c348691376e1e0b301bbd18e585563
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:32:15 +0700

    Menambahkan jarak antar elemen

--- app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -106,5 +107,8 @@ fun DaftarData(
                 }
             }
         }
+
+        // Memberi jarak
+        Spacer(modifier = Modifier.height(20.dp))
     }
 }

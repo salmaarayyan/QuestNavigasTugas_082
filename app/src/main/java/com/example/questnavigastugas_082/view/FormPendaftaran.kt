41743063f797d4e48074333c643b1cf7c97d73eb
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:48:24 +0700

    Menambahkan Box header dengan background ungu dan padding

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -2,8 +2,11 @@ package com.example.questnavigastugas_082.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -28,6 +31,16 @@ fun FormulirPendaftaran(
             .background(Color(0xFFF6E6FF))
             .verticalScroll(rememberScrollState())
         ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color(0xFFB56BFF))
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
+
+        }
 
     }
 }

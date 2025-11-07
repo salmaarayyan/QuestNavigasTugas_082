58991f263872bd2d3484a385098104f38e63bd80
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:28:28 +0700

    Menambahkan header Box dengan judul "List Daftar Peserta"

--- app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
@@ -1,13 +1,22 @@
 package com.example.questnavigastugas_082.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DaftarData(
@@ -24,5 +33,22 @@ fun DaftarData(
             .fillMaxSize()
             .background(Color(0xFFF6E6FF))
             .verticalScroll(rememberScrollState()) // Tambahkan ini agar aman di semua ukuran layar
-    ) {}
+    ) {
+        // Header
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color(0xFFB56BFF))
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            Text(
+                text = "List Daftar Peserta",
+                color = Color.White,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }

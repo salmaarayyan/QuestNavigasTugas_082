2f9b02f95e77956b0f6d4064da83cceccfdc40cf
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 13:55:21 +0700

    Menambahkan teks dan Spacer

--- app/src/main/java/com/example/questnavigastugas_082/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/HomePage.kt
@@ -2,11 +2,16 @@ package com.example.questnavigastugas_082.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,9 +23,9 @@ fun HalamanUtama(
             .fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-
+        horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
+            Text("Selamat Datang", style = MaterialTheme.typography.headlineLarge, fontFamily = FontFamily.Monospace)
+            Spacer(modifier = Modifier.height(32.dp))
     }
 }

d1e2aecadcd3f4836bf1a8900e12bde133d6d73b
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 13:50:27 +0700

    Menambahkan fungsi HalamanUtama dengan parameter onNextButtonClicked

--- app/src/main/java/com/example/questnavigastugas_082/view/Formulir.kt
+++
@@ -1,2 +0,0 @@
-package com.example.questnavigastugas_082.view
-
---
+++ app/src/main/java/com/example/questnavigastugas_082/view/HomePage.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigastugas_082.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HalamanUtama(
+    onNextButtonClicked: () -> Unit
+) {
+
+}

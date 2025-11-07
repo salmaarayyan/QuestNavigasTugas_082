43eb89d3cd711f66739ca3c56624db2d548089ba
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:48:50 +0700

    Menambahkan judul "Formulir Pendaftaran" di header dengan styling

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -10,11 +10,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormulirPendaftaran(
@@ -39,7 +42,13 @@ fun FormulirPendaftaran(
                 .padding(16.dp),
             contentAlignment = Alignment.BottomStart
         ) {
-
+            Text(
+                text = "Formulir Pendaftaran",
+                color = Color.White,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 16.dp)
+            )
         }
 
     }

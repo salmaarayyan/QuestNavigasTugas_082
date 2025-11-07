298ba6a8ab7353ff13c47c1deac0116dcf1e1716
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:27:53 +0700

    Menambahkan Column dengan background ungu muda dan scroll vertikal

--- app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/TampilData.kt
@@ -1,6 +1,13 @@
 package com.example.questnavigastugas_082.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun DaftarData(
@@ -11,4 +18,11 @@ fun DaftarData(
     val jenisKelamin = "Perempuan"
     val status = "Lajang"
     val alamat = "Balikpapan"
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF6E6FF))
+            .verticalScroll(rememberScrollState()) // Tambahkan ini agar aman di semua ukuran layar
+    ) {}
 }

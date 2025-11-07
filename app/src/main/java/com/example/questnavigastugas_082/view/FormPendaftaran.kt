7898941648f99c0d75ede444b8f35e3f5ec4cc6e
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:45:40 +0700

    Menambahkan Column dengan konten di tengah

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -1,12 +1,27 @@
 package com.example.questnavigastugas_082.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirPendaftaran(
     onSubmitButtonClicked: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
+        ) {
+
+    }
 }

2be83f615135ff9440314bbc0af6341ee09ec7d0
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 15:09:33 +0700

    Merevisi code

--- app/src/main/java/com/example/questnavigastugas_082/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_082/Navigasi.kt
@@ -1,3 +1,5 @@
+package com.example.questnavigastugas_082
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable

2d081d6d513bd7ec674b021a37f73afa2fd400fd
salmaarayyan <salmaarayyan@gmail.com>
2025-11-07 14:50:48 +0700

    Menambahkan spacer untuk jarak antar elemen

--- app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_082/view/FormPendaftaran.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -72,6 +73,7 @@ fun FormulirPendaftaran(
                     label = { Text("Nama Lengkap") },
                     modifier = Modifier.fillMaxWidth()
                 )
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
 
